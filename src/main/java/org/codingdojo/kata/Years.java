e495736a5a678ff005c89c788376719e7fb22338
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2022-08-21 12:49:18 +0200

    [LeapYears] Fourth step : 1700 REFACTOR

--- src/main/java/org/codingdojo/kata/Years.java
+++ src/main/java/org/codingdojo/kata/Years.java
@@ -3,8 +3,8 @@ package org.codingdojo.kata;
 public class Years {
 
     public boolean isLeap(int year) {
-        if(year == 1700) return false;
         if(year % 400 == 0) return true;
+        if(year % 100 == 0) return false;
         if(year % 4 == 0) return true;
         return false;
     }
