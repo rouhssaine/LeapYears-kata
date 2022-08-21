b094cd70c67a541d16704ad70de9c207eaded9c2
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2022-08-21 12:40:27 +0200

    [LeapYears] Second step : 2001 REFACTOR

--- src/main/java/org/codingdojo/kata/Years.java
+++ src/main/java/org/codingdojo/kata/Years.java
@@ -3,8 +3,8 @@ package org.codingdojo.kata;
 public class Years {
 
     public boolean isLeap(int year) {
-        if(year == 2001) return false;
-        return true;
+        if(year % 400 == 0) return true;
+        return false;
     }
 
 }
