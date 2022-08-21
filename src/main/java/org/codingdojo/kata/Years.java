f41b3c20cfb11b1f6074a84e450d2b1c66ab6ff8
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2022-08-21 12:29:09 +0200

    [LeapYears] First step : 2000 RED

---
+++ src/main/java/org/codingdojo/kata/Years.java
@@ -0,0 +1,9 @@
+package org.codingdojo.kata;
+
+public class Years {
+
+    public boolean isLeap(int year) {
+        return false;
+    }
+
+}
--- src/test/java/org/codingdojo/kata/LeapYearsTest.java
+++ src/test/java/org/codingdojo/kata/LeapYearsTest.java
@@ -1,4 +1,24 @@
 package org.codingdojo.kata;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
 public class LeapYearsTest {
+
+    @Test
+    void should_be_leap_years_when_year_is_2000() {
+        // GIVEN
+        int year = 2000;
+
+        // WHEN
+        Years years = new Years();
+        boolean result = years.isLeap(year);
+
+        // THEN
+        assertThat(result).isTrue();
+    }
 }
